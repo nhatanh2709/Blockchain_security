4ee9ee422f1f73a0b9da1bfd96a2f37552a42d90
nhatanh2709 <127209960+nhatanh2709@users.noreply.github.com>
2024-02-26 22:52:20 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks:{
+    hardhat:{
+      initialBaseFeePerGas: 0,
+      mining: {
+        auto: false,
+        interval: 3000,
+      }
+    }
+  },
   solidity: "0.8.24",
 };
